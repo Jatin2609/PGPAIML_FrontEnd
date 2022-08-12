68c38a9705d21cb148fc69810d5595716bbfd2ba
Jatin2609 <109457886+Jatin2609@users.noreply.github.com>
2022-08-12 20:55:52 +0530

    Update App.py

--- App.py
+++ App.py
@@ -116,8 +116,8 @@ with st.form("my_form"):
                     df_prediction['probs'] = np.transpose(prob)
                     df_prediction = df_prediction.sort_values(by='probs',ascending = False, ignore_index=True)
                     st.write('The ticket cannot be assigned automatically with a reasonable accuracy. Assigning it to manual assignment team' )
-                    st.write('This ticket is expected to be assigned to group : ', df_prediction.loc[0,'Classes'], 'with a probability of ', df_prediction.loc[0,'probs']\
-                             ,'and, group: ', df_prediction.loc[1,'Classes'], 'with a probability of ', df_prediction.loc[1,'probs'] )
+                    st.write('This ticket is expected to be assigned to group : ', df_prediction.loc[0,'Classes'], 'with a probability of ', np.round_(df_prediction.loc[0,'probs'])\
+                             ,'or, group: ', np.round_(df_prediction.loc[1,'Classes']), 'with a probability of ', df_prediction.loc[1,'probs'] )
         
     
     
